e5f395f1d9d502e68ef83be23544671556b62a37
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-21 09:44:36 +0530

    modify class

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,25 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
 @RestController
+@RequestMapping("./api/users")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	List<User> getAllUsers(){
+		
+		return this.userRepository.findAll();
+	}
+	
 
 }
