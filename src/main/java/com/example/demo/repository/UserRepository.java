0fda281a017785428309a6664abafd98ece1f9f6
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-21 09:29:18 +0530

    add new class ResourceNotFoundException in exception package

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, long>{
+import com.example.demo.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
