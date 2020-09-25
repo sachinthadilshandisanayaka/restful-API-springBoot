948a49b4adcfc776bcfd3fab14a72f8bca440f3d
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-25 16:21:23 +0530

    add new APIException.java class

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,28 @@
 package com.example.demo.exception;
 
-public class GlobalExceptionHandler {
+import java.util.Date;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	// handle specific exception
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request){
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	// handle global exception
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleGlobalException(Exception exception, WebRequest request){
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 }
--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -9,8 +9,7 @@ public class ResourceNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(String message) {
-		super(message);
-		
+		super(message);	
 	}
 	
 }
