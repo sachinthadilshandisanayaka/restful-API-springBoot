1c52471074fb4808512bbc573b14d2e8a1271860
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-25 11:21:36 +0530

    add method to get name by user id

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,24 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 
 @RestController
-@RequestMapping("./api/users")
+@RequestMapping("/api/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
+	// get all user
 	@GetMapping
 	List<User> getAllUsers(){
-		
 		return this.userRepository.findAll();
 	}
 	
+	// get user by id
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable (value = "id") long userId) {
+		return this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found by id :"+ userId));
+	}
 
 }
