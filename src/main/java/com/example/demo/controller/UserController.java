355a972616895a8dad91a86f8720773ae6b17944
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-25 11:03:44 +0530

    database connection

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping
 	List<User> getAllUsers(){
 		
 		return this.userRepository.findAll();
